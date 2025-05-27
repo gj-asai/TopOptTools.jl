4f21e43ecaf555906de5eb67a13be90e4c69f635
Gustavo Asai <gustavojun.asai@gmail.com>
2025-05-27 13:22:12 +0200

    fix shear terms of elasticity tensor

--- src/material.jl
+++ src/material.jl
@@ -7,7 +7,7 @@ end
 function Isotropic2D(; E, nu, ρ=0.0, CO2=0.0)
     S = [1/E -nu/E 0;
         -nu/E 1/E 0;
-        0 0 2(1+nu)/E]
+        0 0 (1+nu)/E]
     C = fromvoigt(SymmetricTensor{4,2,Float64}, inv(S))
     return Material{2,Float64,typeof(C)}(C, ρ, CO2)
 end
@@ -16,9 +16,9 @@ function Isotropic3D(; E, nu, ρ=0.0, CO2=0.0)
     S = [1/E -nu/E -nu/E 0 0 0;
         -nu/E 1/E -nu/E 0 0 0;
         -nu/E -nu/E 1/E 0 0 0;
-        0 0 0 2(1+nu)/E 0 0;
-        0 0 0 0 2(1+nu)/E 0;
-        0 0 0 0 0 2(1+nu)/E]
+        0 0 0 (1+nu)/E 0 0;
+        0 0 0 0 (1+nu)/E 0;
+        0 0 0 0 0 (1+nu)/E]
     C = fromvoigt(SymmetricTensor{4,3,Float64}, inv(S))
     return Material{3,Float64,typeof(C)}(C, ρ, CO2)
 end
@@ -26,7 +26,7 @@ end
 function Orthotropic2D(; El, Et, nult, Glt, ρ=0.0, CO2=0.0)
     S = [1/El -nult/El 0;
         -nult/El 1/Et 0;
-        0 0 1/Glt]
+        0 0 1/2Glt]
     C = fromvoigt(SymmetricTensor{4,2,Float64}, inv(S))
     return Material{2,Float64,typeof(C)}(C, ρ, CO2)
 end
@@ -36,9 +36,9 @@ function Orthotropic3D(; El, Et, nult, Glt, ρ=0.0, CO2=0.0)
     S = [1/El -nult/El -nult/El 0 0 0;
         -nult/El 1/Et -nutt/Et 0 0 0;
         -nult/El -nutt/Et 1/Et 0 0 0;
-        0 0 0 2*(1+nutt)/Et 0 0;
-        0 0 0 0 1/Glt 0;
-        0 0 0 0 0 1/Glt]
+        0 0 0 (1+nutt)/Et 0 0;
+        0 0 0 0 1/2Glt 0;
+        0 0 0 0 0 1/2Glt]
     C = fromvoigt(SymmetricTensor{4,3,Float64}, inv(S))
     return Material{3,Float64,typeof(C)}(C, ρ, CO2)
 end
